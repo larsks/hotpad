a143d825ac1007235f05b45a503c800c442d7f22
Lars Kellogg-Stedman <lars@redhat.com>
2014-03-07 12:20:13 -0500

    missing find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='hotpad',
-      version='1.0',
-      description='High-level wrappers for OpenStack Heat',
-      author='Lars Kellogg-Stedman',
-      author_email='lars@oddbit.com',
-      url='http://github.com/larsks/hotpad',
-      install_requires=open('requirements.txt').readlines(),
-      )
+setup(
+    name='hotpad',
+    version='1.0',
+    description='High-level wrappers for OpenStack Heat',
+    author='Lars Kellogg-Stedman',
+    author_email='lars@oddbit.com',
+    url='http://github.com/larsks/hotpad',
+    install_requires=open('requirements.txt').readlines(),
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'heat-output = hotpad.commands.heat_output:main',
+        ],
+    }
+)
 
